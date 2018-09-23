1b3d172f0deb73a9752f9060c890e7a80e50c788
Cosmo André <sinistro147@gmail.com>
2018-09-23 18:18:25 -0500

    novo path de tradução

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -8,5 +8,23 @@ return [
 	'name' => 'Name',
 	'email' => 'Email Address',
 	'phone' => 'Phone Number',
-	'message' => 'Message'
+	'message' => 'Message',
+
+
+	//about
+	'about_title' => 'About',
+	'about_left' => 'Our focus is to provide a safe and easy to use environment for you to make and receive your payments, quickly, simply and best of all, very easy, everything without bureaucracy',
+	'about_right' => 'Our differential is to provide a wide variety of currencies, a simple use api, integration with several programming languages, you can use in websites, games, applications and so on',
+
+
+	//apresentação da api
+	'api_title' => 'FEW LINES OF CODE',
+	'account_creation' => 'Account Creation',
+	'account_change' => 'Account Change',
+	'balance_transfers' => 'Balance Transfers',
+	'creating_addresses' => 'Creating Addresses',
+	'button' => 'View Documentation',
+	'quick_notifications' => 'Quick Notifications',
+	'transaction_query' => 'Transaction Query',
+	'withdrawal_of_fundos' => 'Withdrawal Of Funds'
 ];
--- resources/lang/pt/home.php
+++ resources/lang/pt/home.php
@@ -9,4 +9,22 @@ return [
 	'email' => 'Endereço de Email',
 	'phone' => 'Numero de Telefone',
 	'message' => 'Mensagem'
+
+
+	//about
+	'about_title' => 'Sobre',
+	'about_left' => 'Nosso foco é fornecer um ambiênte seguro e de fácil uso para você fazer e receber seus pagamentos, de forma rápida, simples e o melhor de tudo, muito fácil, tudo sem burocracia',
+	'about_right' => 'Nosso diferencial é fornecer uma grande variedade de moedas, uma api de uso simples, integração com diversas linguagens de programações, você pode usar em sites, jogos, aplicativos e etc.',
+
+
+	//apresentação da api
+	'api_title' => 'ALGUMAS LINHAS DE CÓDIGO',
+	'account_creation' => 'Criação De Conta',
+	'account_change' => 'Alteração De Conta',
+	'balance_transfers' => 'Transferências De Saldo',
+	'creating_addresses' => 'Creating Addresses',
+	'button' => 'Ver Documentação',
+	'quick_notifications' => 'Notificações Rápidas',
+	'transaction_query' => 'Consulta De Transações',
+	'withdrawal_of_fundos' => 'Retirada de Fundos'
 ];
