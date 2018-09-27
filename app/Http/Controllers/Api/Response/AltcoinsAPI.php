c4e86769f0a092ba73511d14ea1b24cecbd640a3
Cosmo André <sinistro147@gmail.com>
2018-09-27 15:25:57 -0500

    adicionado cache na chamada de api pra altcoins

--- app/Http/Controllers/Api/Response/AltcoinsAPI.php
+++ app/Http/Controllers/Api/Response/AltcoinsAPI.php
@@ -7,30 +7,40 @@ use Altcoins;
 use Api;
 use Response;
 use User;
+use Cache;
 
 
 class AltcoinsAPI extends \Controller {
+
+	private $minutes = 120;
 
 	public function all($api = '', $pin_key = '') {
 		$user = Api::User($api, $pin_key);
 
 		if ($user) {
-			$coins = [];
-			foreach (Altcoins() as $data) {
+			if (!Cache::has('altcoins')) {
+				$coins = [];
+				foreach (Altcoins() as $data) {
 
-				$coins[$data['coin']] = [
-					'coin' => $data['coin'],
-					'name' => $data['name'],
-					'site' => $data['site'],
-					'fees' => [
-						'deposit' => floatval($data['fees']['deposit']),
-						'withdrawal' => floatval($data['fees']['withdrawal']),
-					]
+					$coins[$data['coin']] = [
+						'coin' => $data['coin'],
+						'name' => $data['name'],
+						'site' => $data['site'],
+						'fees' => [
+							'deposit' => floatval($data['fees']['deposit']),
+							'withdrawal' => floatval($data['fees']['withdrawal']),
+						]
 
-				]; 
+					]; 
+				}
+
+				$array = ['status' => 'success', 'data' => $coins];
+
+				Cache::put('altcoins', $array, $this->minutes);
+
 			}
 
-			return Response::Json(['status' => 'success', 'data' => $coins]);
+			return Response::Json(Cache::get('altcoins'));
 		}
 
 		return Response::Json(InvalidAPI());
@@ -45,19 +55,23 @@ class AltcoinsAPI extends \Controller {
 
 			$coin = strtolower($coin);
 
-			$coin = Altcoin($coin);
-			
-			if ($coin and !empty($coin['id'])) {
+			if (!Cache::has('altcoin_'.$coin)) {
 
-				unset($coin['id'], $coin['module'], $coin['fees']['withdrawal_api'], $coin['fees']['deposit_api'], $coin['rpc']);
-				$coin = [
-					'status' => 'success',
-					'data' => $coin
-				];
+				$coinx = Altcoin($coin);
+				
+				if ($coinx and !empty($coin['id'])) {
 
-				return Response::Json($coin);
-			} else
-				return Response::Json(InvalidCoin());
+					unset($coinx['id'], $coinx['module'], $coinx['fees']['withdrawal_api'], $coinx['fees']['deposit_api'], $coinx['rpc']);
+					$coinx = [
+						'status' => 'success',
+						'data' => $coinx
+					];
+
+					Cache::put('altcoin_'.$coin, $coinx, $this->minutes);
+				} else
+					Cache::put('altcoin_'.$coin, InvalidCoin(), $this->minutes);
+
+			return Response::Json(Cache::get('altcoin_'.$coin));
 		}
 
 		return Response::Json(InvalidAPI());
