7133795bb1ba509c3da4a25bec48ca2b9f0b932c
Cosmo André <sinistro147@gmail.com>
2018-10-09 00:40:07 -0500

    fix

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -5,8 +5,6 @@ $(function() {
         try {
             var ok = document.execCommand('copy');
             if (ok) {
-                 //alertify.set('position', 'bottom-right');
-                alertify.logPosition("bottom right");
                 alertify.success('Copied to clipboard');
                 $(this).unselect();
 
