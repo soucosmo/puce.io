0c33d5995ee20c2243e14447d74cca2514975186
Cosmo André <cosmo@ecwsa.com.br>
2024-08-20 13:13:28 -0300

    fix

--- app/Http/Controllers/Test.php
+++ app/Http/Controllers/Test.php
@@ -15,38 +15,6 @@ use Auth;
 class Test extends \Controller {
 
 	public function index() {
-/*
-
-		$start= microtime(true);
-
-	// Script que desejas medir o tempo
-		echo '<pre>';
-		print_r(Altcoins());
-
-		$end= microtime(true);
-
-		$time = abs($start - $end);
-
-		echo '<br>'.number_format($time,25) . '<br>';
-		echo memory_get_usage();*/
-	//	echo Crypt::encryptString('D12EDDDD7DF0192EEC538DD8140C38468A6F8D52');
-
-		//User::Find(1)->login()->create([]);
-
-		/*
-		$Puce = new Puce('D12EDDDD7DF0192EEC538DD8140C38468A6F8D52', true);
-
-		print_r(
-			$Puce->withdrawal_test('btc', 0.01, 'cosmodasdas', 'http://google.com')
-		);
-		*/
- 		
- 		//$nome = 'Cosmo';
-
- 		echo Hash::make('Cosmo9able');
-
+		//
 	}
-
-	
-
 }
